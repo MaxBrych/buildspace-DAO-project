ff51ce0556ee88fce812d3ae155bd662ade4777a
MaxBrych <max.brych03@gmail.com>
2023-05-14 23:35:25 +0200

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,70 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import {
+  useAddress,
+  ConnectWallet,
+  Web3Button,
+  useContract,
+  useNFTBalance,
+} from "@thirdweb-dev/react";
+import { useState, useEffect, useMemo } from "react";
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
+const App = () => {
+  // Use the hooks thirdweb give us.
+  const address = useAddress();
+  console.log("👋 Address:", address);
+  // Initialize our Edition Drop contract
+  const editionDropAddress = "0x7f2BfBf0E6904b5B6Facec197C64b8eB4b1aBeC1";
+  const { contract: editionDrop } = useContract(
+    editionDropAddress,
+    "edition-drop"
+  );
+  // Hook to check if the user has our NFT
+  const { data: nftBalance } = useNFTBalance(editionDrop, address, "0");
 
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
+  const hasClaimedNFT = useMemo(() => {
+    return nftBalance && nftBalance.gt(0);
+  }, [nftBalance]);
 
-        <div className={styles.connect}>
+  // This is the case where the user hasn't connected their wallet
+  // to your web app. Let them call connectWallet.
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to NarutoDAO</h1>
+        <div className="btn-hero">
           <ConnectWallet />
         </div>
+      </div>
+    );
+  }
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
+  // Render mint nft screen.
+  return (
+    <div className="mint-nft">
+      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <p>
+        balance: {nftBalance?.toString()} <br />
+      </p>
+      <br />
+      <div className="btn-hero">
+        <Web3Button
+          contractAddress={editionDropAddress}
+          action={(contract) => {
+            contract?.erc1155.claim(0, 1);
+          }}
+          onSuccess={() => {
+            console.log(
+              `🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop?.getAddress()}/0`
+            );
+          }}
+          onError={(error) => {
+            console.error("Failed to mint NFT", error);
+          }}
+        >
+          Mint your NFT (FREE)
+        </Web3Button>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default App;
